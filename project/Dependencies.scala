b5c620ef7ac642f535680c742236d89f187875e5
Matteo Castellucci <matteo.castellucci@outlook.com>
2023-07-28 16:25:55 +0200

    build: update all akka dependencies at once

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,15 +14,15 @@ object Dependencies {
 
   lazy val akka = "com.typesafe.akka" %% "akka-actor-typed" % "2.8.3"
 
-  lazy val akkaStream = "com.typesafe.akka" %% "akka-stream-typed" % "2.8.0"
+  lazy val akkaStream = "com.typesafe.akka" %% "akka-stream-typed" % "2.8.3"
 
   lazy val akkaHttp = "com.typesafe.akka" %% "akka-http" % "10.5.0"
 
-  lazy val akkaStreamTestkit = "com.typesafe.akka" %% "akka-stream-testkit" % "2.8.0" % Test
+  lazy val akkaStreamTestkit = "com.typesafe.akka" %% "akka-stream-testkit" % "2.8.3" % Test
 
   lazy val akkaHttpTestkit = "com.typesafe.akka" %% "akka-http-testkit" % "10.5.0" % Test
 
-  lazy val akkaTestkit = "com.typesafe.akka" %% "akka-actor-testkit-typed" % "2.8.0" % Test
+  lazy val akkaTestkit = "com.typesafe.akka" %% "akka-actor-testkit-typed" % "2.8.3" % Test
 
   lazy val core = "io.github.cake-lier" %% "tuples-space-core" % "1.0.1"
 }
